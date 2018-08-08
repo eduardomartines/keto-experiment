a47d576a7e6a758362f82971b73793f9e8552440
Eduardo Martines <eduardo.martines@totvs.com.br>
2018-08-08 17:35:00 -0300

    adds authorization tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 docker-compose down; docker-compose run --rm keto-migrate
 
-docker-compose run --rm test
+docker-compose run --rm test rspec spec/integration/policy_creation_spec.rb
+
+ocker-compose run --rm test rspec spec/integration/policy_authorization_spec.rb
--- spec/integration/policy_authorization_spec.rb
+++ spec/integration/policy_authorization_spec.rb
@@ -2,13 +2,35 @@ require 'net/http'
 require 'json'
 
 describe 'Policies' do
+  let(:uri) { URI('http://keto:4466/warden/subjects/authorize') }
+  let(:http) { Net::HTTP.new(uri.host, uri.port) }
+
   describe 'authorization' do
+    before do
+      request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+      request.body = '{
+        "action": "action-foo",
+        "context": { "conditionKeyFoo": "conditionValueFoo" },
+        "resource": "resource-foo:bar",
+        "subject": "' + subject + '"
+      }'
+      @response = http.request(request)
+    end
+
     context 'when access is granted' do
-      it 'returns true'
+      let(:subject) { 'subject-foo' }
+
+      it 'returns true' do
+        expect(@response.body).to eq("{\"sub\":\"subject-foo\",\"allowed\":true}")
+      end
     end
 
     context 'when access is denied' do
-      it 'returns false'
+      let(:subject) { 'subject-bar' }
+
+      it 'returns false' do
+        expect(@response.body).to eq("{\"allowed\":false}")
+      end
     end
   end
 end
